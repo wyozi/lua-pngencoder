e44997e18b2aad089938c994c685da1fd94134a1
David Vogel <Dadido3@aol.com>
2019-10-27 00:24:29 +0200

    Add RGBA mode

--- README.md
+++ README.md
@@ -5,7 +5,8 @@ Extremely simple PNG encoder for Lua. Inspired (read: directly transpiled) from 
 ## Usage
 
 Import into your project, change usage of `bit` library to whatever suits your platform (by default it uses LuaJIT compatible `bit` library), use like this:
-```lua
+
+``` lua
 local encode = require "pngencoder"
 
 local png = encode(64, 64) -- width, height
@@ -20,3 +21,18 @@ local data = png.output -- table containing raw PNG data
 
 Note that the `output` table is populated in streaming fashion as you call `write`. You __must__ write all
 the `width * height * 3` bytes before doing anything with the output to make sure the footer is properly written and the PNG file is complete. You can use the `done` flag to make sure you're done writing.
+
+It's also possible to write RGBA data:
+
+``` lua
+local encode = require "pngencoder"
+
+local png = encode(64, 64, PNG_COLOR_TYPE_RGBA) -- width, height
+png:write { 0xFF, 0, 0, 0x80 } -- RGB(255, 0, 0, 128) pixel at 0x0 (half transparent)
+png:write { 0, 0xFF, 0, 0 }    -- RGB(0, 255, 0, 0)   pixel at 1x0 (fully transparent)
+-- TODO: write rest of the pixels to fill rest of the 64x64 canvas
+
+assert(png.done) -- just a failsafe to make sure we've filled the whole allocated PNG space
+
+local data = png.output -- table containing raw PNG data
+```
--- pngencoder.lua
+++ pngencoder.lua
@@ -1,6 +1,9 @@
 local Png = {}
 Png.__index = Png
 
+PNG_COLOR_TYPE_RGB = 2
+PNG_COLOR_TYPE_RGBA = 6
+
 local DEFLATE_MAX_BLOCK_SIZE = 65535
 
 local function putBigUint32(val, tbl, index)
@@ -121,11 +124,20 @@ function Png:adler32(data, index, len)
     self.adler = bit.bor(bit.lshift(s2, 16), s1)
 end
 
-local function begin(width, height)
-    local state = setmetatable({ width = width, height = height, done = false, output = {} }, Png)
+local function begin(width, height, colorType)
+    local state = setmetatable({ width = width, height = height, done = false, output = {}, colorType = colorType or PNG_COLOR_TYPE_RGB }, Png)
+
+    local bytesPerPixel
+    if state.colorType == PNG_COLOR_TYPE_RGB then
+        bytesPerPixel = 3
+    elseif state.colorType == PNG_COLOR_TYPE_RGBA then
+        bytesPerPixel = 4
+    else
+        error("Invalid colorType")
+    end
 
     -- Compute and check data siezs
-    state.lineSize = width * 3 + 1
+    state.lineSize = width * bytesPerPixel + 1
     -- TODO: check if lineSize too big
 
     state.uncompRemain = state.lineSize * height
@@ -146,7 +158,7 @@ local function begin(width, height)
         0x49, 0x48, 0x44, 0x52,
         0, 0, 0, 0,  -- 'width' placeholder
         0, 0, 0, 0,  -- 'height' placeholder
-        0x08, 0x02, 0x00, 0x00, 0x00,
+        0x08, state.colorType, 0x00, 0x00, 0x00,
         0, 0, 0, 0,  -- IHDR CRC-32 placeholder
         -- IDAT chunk
         0, 0, 0, 0,  -- 'idatSize' placeholder
